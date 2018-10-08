94f557c051d759e6e03895dd78771613eeaa5984
AzySir <azy.sir@gmail.com>
2018-10-08 14:55:27 +1100

    New QuizEnd Component

---
+++ src/QuizEnd.js
@@ -0,0 +1,18 @@
+/* QuizEnd.js */
+import React, { Component } from 'react';
+
+class QuizEnd extends Component { 
+    
+    render() {
+
+        const isQuizEnd;
+        return (
+            <div>
+                <p>Thanks for playing!</p>
+                <a href=''>Reset Quiz</a>
+            </div>
+        )
+    }
+}
+
+export default QuizEnd
