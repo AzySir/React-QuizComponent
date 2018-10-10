f47781245eead88cbee91712d3432b67bbe2c9ca
AzySir <azy.sir@gmail.com>
2018-10-10 17:47:56 +1100

    QuizQuestion.js Question 7 - Added buttonText to the function

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -3,9 +3,8 @@ import React, { Component } from 'react'
 import QuizQuestionButton from './QuizQuestionButton.js'
 
 class QuizQuestion extends Component {
-    handleClick() {
-        const buttonText = "hello";
-        if (this.props.button_text === this.props.quiz_question.answer) {
+    handleClick(buttonText) {
+        if (buttonText === this.props.quiz_question.answer) {
             this.props.showNextQuestionHandler();
         }
     }
