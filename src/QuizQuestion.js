22640772a5cbdc6ff6a2b50cdf829ed8afa79206
AzySir <azy.sir@gmail.com>
2018-10-10 16:34:25 +1100

    QuizQuestionButton entered onClick Handlers & QuizQuestion.js entered clickHandler functions

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -3,6 +3,11 @@ import React, { Component } from 'react'
 import QuizQuestionButton from './QuizQuestionButton.js'
 
 class QuizQuestion extends Component {
+    handleClick() {
+        const buttonText;
+
+
+    }
     render () {
         return (
         <main>
@@ -12,9 +17,8 @@ class QuizQuestion extends Component {
             <section className="buttons">
                 <ul>
                     {this.props.answer_options.map((answer_option,index) => { 
-                        return <QuizQuestionButton button_text={answer_option} key={index} />
+                        return <QuizQuestionButton button_text={answer_option} key={index} clickHandler={this.handleClick.bind(this)} />
                     })}
-                    
                 </ul>
             </section>
         </main>
