d13e124c759e1a66db48086e2aadd994d82a1626
AzySir <azy.sir@gmail.com>
2018-10-10 17:14:03 +1100

    Question 6 - Added more to the handleClick function

--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -4,6 +4,9 @@ import React, { Component } from 'react';
 class QuizQuestionButton extends Component {
     handleClick () {
         this.props.clickHandler(this.props.button_text)
+        if (this.props.button_text === this.props.quiz_question.answer) {
+            this.props.showNextQuestionHandler();
+        }
     }
     render () {
         return(
