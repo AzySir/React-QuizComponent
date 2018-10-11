af0393ad08028356ac33575ab851a877ba1f5ed8
AzySir <azy.sir@gmail.com>
2018-10-11 15:38:03 +1100

    Task 2 - Added setState

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -5,6 +5,10 @@ import QuizQuestionButton from './QuizQuestionButton.js'
 class QuizQuestion extends Component {
     constructor(props) {
         super(props);
+
+        this.setState({
+            incorrectAnswer: false
+        })
     }
     handleClick(buttonText) {
         if (buttonText === this.props.quiz_question.answer) {
